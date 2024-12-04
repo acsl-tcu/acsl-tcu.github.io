41a509b09c409e1ad7fcba4150c58ec2a35897e2
kasekiguchi <kasekiguchi@gmail.com>
2024-12-04 14:23:25 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import Image from "next/image";
-import styles from "./page.module.css";
+//import Image from "next/image";
+//import styles from "./page.module.css";
 import { useEffect, useState } from 'react';
 import React from 'react';
 import { Button } from 'react-bootstrap';
