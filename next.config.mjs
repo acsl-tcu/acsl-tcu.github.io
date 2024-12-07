dc8b6e168eca7f430b18879b503c320a41a3ad79
kasekiguchi <kasekiguchi@gmail.com>
2024-12-08 04:35:34 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,6 @@
 * @type {import('next').NextConfig}
 */
 
-import path from 'path';
-export default {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(process.cwd()); // __dirnameの代わりにprocess.cwd()を使用 
-    return config;
-  },
-};
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
