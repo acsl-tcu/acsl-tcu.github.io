5086225777259b30c0e260cd8a54682df7e6241f
kasekiguchi <kasekiguchi@gmail.com>
2024-12-08 02:33:57 +0900

    Update AppAppBar.tsx

--- app/blog/components/AppAppBar.tsx
+++ app/blog/components/AppAppBar.tsx
@@ -28,9 +28,7 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   //   : alpha(theme.palette.background.default, 0.4),
   // boxShadow: (theme.vars || theme).shadows[1],
   borderColor: theme.palette.divider,
-  backgroundColor: theme.vars
-    ? `rgba(${theme.vars.palette.background.defaultChannel} / 0.4)`
-    : alpha(theme.palette.background.default, 0.4),
+  backgroundColor: alpha(theme.palette.background.default, 0.4),
   boxShadow: theme.shadows[1],
   padding: '8px 12px',
 }));
