0463e80e30d429ad3680a3f60a932ec02538886d
kasekiguchi <kasekiguchi@gmail.com>
2024-12-08 04:54:52 +0900

    Create error.tsx

---
+++ app/error.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ErrorPage = ({ statusCode }: { statusCode: number }) => {
+  return (
+    <div>
+      <h1>{statusCode} エラーが発生しました。</h1>
+      <p>申し訳ありませんが、ページを表示できませんでした。</p>
+    </div>
+  );
+};
+
+ErrorPage.getInitialProps = ({ res, err }: { res: any; err: any }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default ErrorPage;
