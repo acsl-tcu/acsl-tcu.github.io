7012b22e10962a3ee31c34019b40921c681cdb6d
kasekiguchi <kasekiguchi@gmail.com>
2024-12-08 04:38:21 +0900

    Update page.tsx

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -5,10 +5,13 @@ import { useI18nContext } from '@/contexts/i18nContext';
 export default function Hello() {
   const { locale, messages } = useI18nContext();
   return (
-    <h1>{messages.hello.title}</h1>
+    <div>
+      <h1>{messages.hello.title}</h1>
+      <p>{`Current locale: ${locale}`}</p>
+    </div>
   );
 }
 
 //    <div>
 // <h1>{messages.hello.greeting("Visitor")}</h1>
-// <p>{`Current locale: ${locale}`}</p> </div >
+//
