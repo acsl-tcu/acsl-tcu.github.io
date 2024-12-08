b05b8ad164fcacb97ea3183444429255e859adc7
kasekiguchi <kasekiguchi@gmail.com>
2024-12-09 00:47:35 +0900

    Update i18nContext.tsx

--- contexts/i18nContext.tsx
+++ contexts/i18nContext.tsx
@@ -15,12 +15,12 @@ import { messagesMap } from '@/constants/i18n';
 // Define the context type 
 interface I18nContextType { locale: Locale; messages: Messages; }
 // Create the context 
-const I18nContext = createContext<I18nContextType | undefined>(undefined);
+const I18nContext = createContext<I18nContextType | undefined>({ locale: 'en', messages: { hello: { title: "hoge", greeting: (i) => ("hello" + i) } } });
 
 export const useI18nContext = () => {
   const context = useContext(I18nContext);
   if (!context) {
-    throw new Error('Failed to retrieve I18nContext.');
+    throw new Error('Failed to retrieve I18nContext.', context);
   }
   return context;
 };
