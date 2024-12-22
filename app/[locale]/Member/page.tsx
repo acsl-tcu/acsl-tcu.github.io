e8e83e5ffdc2ffa07852ff1022e04d0acb527a48
kasekiguchi <kasekiguchi@gmail.com>
2024-12-23 00:31:17 +0900

    Update page.tsx

--- app/[locale]/Member/page.tsx
+++ app/[locale]/Member/page.tsx
@@ -56,8 +56,8 @@ const MemberTable: React.FC<{ year: number }> = ({ year }) => {
   // console.log(`Member data:  ${rows}`); // 入れると無限ループ：なぜ？
   // フィルタリング 
   // const staffs = rows.filter(member => member.grade !== 'B4' && member.grade !== 'M1' && member.grade !== 'M2');
-  const staffs = rows.filter(member => !["B4", "M1", "M2", "D1", "D2", "D2", "D3", "D4", "D5", "D6", "D7"].includes(member.grade));
-  const doctor_students = rows.filter(member => ["D1", "D2", "D2", "D3", "D4", "D5", "D6", "D7"].includes(member.grade));
+  const staffs = rows[0].filter((member: Member) => !["B4", "M1", "M2", "D1", "D2", "D2", "D3", "D4", "D5", "D6", "D7"].includes(member.grade));
+  const doctor_students = rows[0].filter((member: Member) => ["D1", "D2", "D2", "D3", "D4", "D5", "D6", "D7"].includes(member.grade));
   return (
     <div>
       {/* <pre>{JSON.stringify(rows, null, 2)}</pre> */}
