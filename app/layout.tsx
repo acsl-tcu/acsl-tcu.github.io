ebef995ad4ee006b3e7731e63a18268efeb9e598
kasekiguchi <kasekiguchi@gmail.com>
2025-02-25 14:14:09 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+//import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
 import { ReactNode } from 'react';
 import "./globals.css";
