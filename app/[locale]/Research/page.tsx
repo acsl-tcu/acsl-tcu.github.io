ffcfab9b660ab77612c4236220e63162956af385
kasekiguchi <kasekiguchi@gmail.com>
2025-05-06 23:45:07 +0900

    trial

---
+++ app/[locale]/Research/page.tsx
@@ -0,0 +1,25 @@
+// pages/[title].tsx
+'use client'
+// import { useRouter } from 'next/router';
+// import useDB from '@/hooks/useDB';
+// import MediaDisplay from '@/app/components/MediaDisplay';
+
+const MediaPage = () => {
+  // const router = useRouter();
+  // const { title } = router.query;
+  // const lang = 'en'; // 言語は動的に変更できます
+  // const target = 'application'; // ターゲットも適切に設定します
+
+  // const { rows, error } = useDB(table, year);
+
+  // return (
+  //   <div>
+  //     <h1>{rows.title}</h1>
+  //     <p>{rows.abstract}</p>
+  //     <MediaDisplay figures={rows.figures} />
+  //   </div>
+  // );
+  return (<>Under construction</>);
+};
+
+export default MediaPage;
---
+++ app/components/MediaDisplay.tsx
@@ -0,0 +1,58 @@
+// components/MediaDisplay.tsx
+import React from 'react';
+
+interface MediaProps {
+  src: string;
+  caption: string;
+  index: number;
+}
+
+const Media: React.FC<MediaProps> = ({ src, caption, index }) => {
+  const isImage = /\.(jpg|png|tiff|gif)$/.test(src);
+  const isVideo = /\.(mp4|avi)$/.test(src);
+
+  if (isImage) {
+    return (
+      <div className="article-content-box">
+        <img className="article-figure" src={`img/${src}`} alt={caption} />
+        <legend style={{ textAlign: 'justify' }}>
+          Fig. {index} : {caption}
+        </legend>
+      </div>
+    );
+  } else if (isVideo) {
+    const videoType = src.match(/.*\.(mp4|avi)$/)?.[1];
+    return (
+      <div className="article-content-box">
+        <video className="article-figure" src={`img/${src}`} controls type={`video/${videoType}`}>
+          <p>動画を再生するにはvideoタグをサポートしたブラウザが必要です。</p>
+        </video>
+        <legend>Movie {index} : {caption}</legend>
+      </div>
+    );
+  }
+  return null;
+};
+
+interface MediaDisplayProps {
+  figures: { src: string; caption: string }[];
+}
+
+const MediaDisplay: React.FC<MediaDisplayProps> = ({ figures }) => {
+  return (
+    <div className="article-content">
+      <div className="article-content-wrap">
+        {figures.slice(0, 2).map((fig, index) => (
+          <Media key={index} src={fig.src} caption={fig.caption} index={index + 1} />
+        ))}
+      </div>
+      <div className="article-content-wrap">
+        {figures.slice(2, 4).map((fig, index) => (
+          <Media key={index + 2} src={fig.src} caption={fig.caption} index={index + 3} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MediaDisplay;
