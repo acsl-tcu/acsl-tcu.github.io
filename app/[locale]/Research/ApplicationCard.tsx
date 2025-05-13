43e0736fc020b60d58812aea06150922595f58d6
Kazuma SEKIGUCHI <ksekiguc@tcu.ac.jp>
2025-05-13 16:36:04 +0900

    research tab

--- app/[locale]/Research/ApplicationCard.tsx
+++ app/[locale]/Research/ApplicationCard.tsx
@@ -5,7 +5,9 @@ const Card: React.FC<CardProps> = ({ items, set }) => {
   return (
     <li className="px-4 py-4 shadow-lg shadow-gray-300 rounded-2xl" onClick={() => set(items)}>
       <a href="#">
-        <h3>{items[0].name}</h3>
+        <h3>{items[0].name.split('_')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ')}</h3>
         <div className="mt-3">
           <Image
             src={`/images/${items[0].name}.jpg`} alt={items[0].name}
