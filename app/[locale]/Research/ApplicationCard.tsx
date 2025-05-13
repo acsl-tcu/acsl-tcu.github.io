b5d0fb03c41bcd17968c1d547e2e75e739ffadd2
Kazuma SEKIGUCHI <ksekiguc@tcu.ac.jp>
2025-05-13 15:37:23 +0900

    research tab

--- app/[locale]/Research/ApplicationCard.tsx
+++ app/[locale]/Research/ApplicationCard.tsx
@@ -3,7 +3,7 @@ import { CardProps } from "./ResearchInterface";
 
 const Card: React.FC<CardProps> = ({ items, set }) => {
   return (
-    <li className="px-4 py-4 shadow-lg shadow-gray-300 rounded-2xl" onClick={set}>
+    <li className="px-4 py-4 shadow-lg shadow-gray-300 rounded-2xl" onClick={() => set(items)}>
       <a href="#">
         <h3>{items[0].name}</h3>
         <div className="mt-3">
