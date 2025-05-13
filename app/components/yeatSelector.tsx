075435796da3c8287e6c49c0353d3e5c90b4457e
Kazuma SEKIGUCHI <ksekiguc@tcu.ac.jp>
2025-05-14 01:30:25 +0900

    research tab

--- app/components/yeatSelector.tsx
+++ app/components/yeatSelector.tsx
@@ -22,30 +22,17 @@ const YearSelector: React.FC<TextProps> = ({ texts, dispYear, setDispYear, hrefs
   return (
     <div className="bg-white border border-gray-200 shadow-md rounded-4sm p-4">
       <div className="flex items-end gap-3 m-2">
-        <div className="relative">
-          <input
-            type="text"
-            id="demo-select-small"
-            className="peer block w-full border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            placeholder=" "
-          />
-          <label
-            htmlFor="demo-select-small"
-            className="absolute left-2 top-2 text-sm text-gray-500 peer-placeholder-shown:top-6 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 transition-all duration-200"
-          >
-            Age
-          </label>
-        </div>
         {/* 年セレクター */}
-        <div className="flex-col ">
-          <label htmlFor="year-select" className="block mb-1 text-sm font-semibold text-gray-700">
+        <div className="flex-col relative">
+          <label htmlFor="year-select" className="block mb-1 text-sm font-semibold text-gray-700 
+          absolute left-2 top-0 bg-white">
             Select Year
           </label>
           <select
             id="year-select"
             value={dispYear}
             onChange={handleChange}
-            className=" rounded-sm border border-gray-300 bg-white px-3 py-2 text-sm shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+            className=" rounded-sm border border-gray-300 bg-white px-3 py-2 text-sm shadow-sm w-20 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           >
             {year_list.map((year) => (
               <option key={year} value={year}>
