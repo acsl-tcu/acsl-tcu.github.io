8a2a2b0634524d171f8d0f8cd4e79071b768913d
kasekiguchi <kasekiguchi@gmail.com>
2025-10-30 08:59:03 +0900

    Debug

--- app/downloads/gen_code.js
+++ app/downloads/gen_code.js
@@ -2,7 +2,17 @@ import QRCode from 'qrcode';
 
 const googleCalendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE&text=第69回自動制御連合講演会&dates=20261107T000000Z/20261109T000000Z&location=東京都市大学世田谷キャンパス〒158-8557東京都世田谷区玉堤１丁目２８−１&trp=false';
 
-QRCode.toDataURL(googleCalendarUrl, (err, url) => {
-  if (err) console.error(err);
-  else console.log(url);  // 画像のDataURLが得られるので、imgタグのsrcにセット可能
+// QRCode.toDataURL(googleCalendarUrl, (err, url) => {
+//   if (err) console.error(err);
+//   else console.log(url);  // 画像のDataURLが得られるので、imgタグのsrcにセット可能
+// });
+
+QRCode.toFile('calendar_event_qr.png', googleCalendarUrl, {
+  errorCorrectionLevel: 'L'  // L, M, Q, H から選択
+}, err => {
+  if (err) {
+    console.error('QRコード生成失敗:', err);
+  } else {
+    console.log('QRコード画像 calendar_event_qr.png が生成されました');
+  }
 });
