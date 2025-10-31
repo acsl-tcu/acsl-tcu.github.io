3c5b063837263df918203a4cd8510723dd8ace28
kasekiguchi <kasekiguchi@gmail.com>
2025-11-01 07:58:00 +0900

    rengo69 カレンダー

--- app/downloads/gen_code.js
+++ app/downloads/gen_code.js
@@ -1,6 +1,10 @@
 import QRCode from 'qrcode';
+// GoogleカレンダーのイベントURLを生成
+// 使い方
+// node --experimental-modules app/downloads/gen_code.js
 
-const googleCalendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE&text=第69回自動制御連合講演会&dates=20261107T000000Z/20261109T000000Z&location=東京都市大学世田谷キャンパス〒158-8557東京都世田谷区玉堤１丁目２８−１&trp=false';
+// const googleCalendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE&text=第69回自動制御連合講演会&dates=20261107T000000Z/20261109T000000Z&location=東京都市大学世田谷キャンパス〒158-8557東京都世田谷区玉堤１丁目２８−１&description=https://rengo69.sice.jp/&trp=false';
+const googleCalendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE&text=第69回自動制御連合講演会&dates=20261107/20261109&location=東京都市大学世田谷キャンパス〒158-8557東京都世田谷区玉堤１丁目２８−１&description=https://rengo69.sice.jp/&trp=false';
 
 // QRCode.toDataURL(googleCalendarUrl, (err, url) => {
 //   if (err) console.error(err);
